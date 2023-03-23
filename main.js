2cab4aa4bfe776dd391a572d5be3f1e5f884fcc1
LeDucViet14 <ducvietle1404@gmail.com>
2023-03-23 08:15:49 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function onload1(){
+    alert('Đây là Trang Chủ');
+}
+
+function onload2(){
+    alert('Đây là Giới Thiệu')
+}
+
+function onload3(){
+    alert('Đây là Đặt Khóa Học')
+}
+
+function onclick(){
+    alert('Đăng kí thành công');
+}
